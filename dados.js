a94a720e8b636f4717743dce6c742d751bee818d
robertobhmg <159694606+robertobhmg@users.noreply.github.com>
2024-09-06 19:17:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,30 @@
+let dados = [
+    {titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, conhecida como a Fadinha, é uma das mais jovens e talentosas skatistas do mundo. Com apenas 14 anos, ela já conquistou diversos títulos e se tornou um ícone para a nova geração de skatistas.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags:"skate fada sk8"
+    },
+
+    {titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Nascida em Guarulhos, a atleta conquistou o país com sua determinação e talento, superando diversas lesões para alcançar o topo do pódio em competições internacionais.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags:"judo judoka"
+    },
+
+    {titulo: "Beatriz Souza",
+    descricao: "A campeã do judô Beatriz Souza é uma judoca brasileira que conquistou o país com sua força e determinação. Com sua medalha de ouro nas Olimpíadas, ela se tornou um exemplo para muitos jovens atletas.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags:"ginastica ouro"
+    }
+
+
+
+
+
+
+
+
+    
+    
+];
+
